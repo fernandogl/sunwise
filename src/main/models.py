8144bfa207914bb6ca3c01173937a00dba04a3b6
Fernando Guerrero <fguerrero11@gmail.com>
2019-01-04 13:38:59 -0600

    Creacion de modelo 'Url' para guardar las URLs y el codigo acortado

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+import string
+import random
 
 # Create your models here.
+
+
+class Url( models.Model ):
+    """
+    Guarda URLs completas u originales y genera un codigo para cada nueva URL 
+    con una longitud maxima de 6 caracteres, comprendiendo de letras 
+    mayusculas y minusculas asi como numeros.
+
+    """
+    original = models.URLField(max_length=2000, unique=True)
+    codigo = models.CharField(max_length=6, unique=True)
+    creado = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.original
+
+    def save(self, *args, **kwargs):
+        # Al crear el registro, se genera un valor aleatorio para el atributo
+        # 'codigo'
+        if not self.id:
+            opciones = ( string.ascii_uppercase 
+                        + string.ascii_lowercase 
+                        + string.digits )
+            self.codigo = ''.join(random.choices(opciones, k=6))
+        super().save(*args, **kwargs)
