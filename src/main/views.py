806dca87432f7e426d7aeb8b9c2e67c118812f45
Fernando Guerrero <fguerrero11@gmail.com>
2019-01-06 15:23:09 -0600

    Vistas para redigir url y para renderizar plantilla con listado de urls

--- src/main/views.py
+++ src/main/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseRedirect
+from django.views.generic import ListView
 from rest_framework import permissions, viewsets
 
 from .models import Url
 from .serializers import UrlSerializer
+
 
 class UrlViewSet(viewsets.ModelViewSet):
     # permission_classes = (permissions.IsAuthenticated, )
 
     queryset = Url.objects.all()
     serializer_class = UrlSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        if isinstance(kwargs.get('data', {}), list):
+            kwargs['many'] = True
+        return super().get_serializer(*args, **kwargs)
+
+
+def redirige(request, codigo):
+	urls = Url.objects.filter( codigo=codigo )
+	if urls.exists():
+		url = urls.first().original
+		return HttpResponseRedirect( url )
+	else:
+		return HttpResponse( f'No existe una URL con este codigo: {codigo}' )
+
+
+
+class UrlListView(ListView):
+    model = Url
